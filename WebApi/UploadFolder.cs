7ad463c4e5ca349151aed15b305548aa7d85f9d8
Felix Åmark <felix.amark@gmail.com>
2023-07-08 11:21:15 +0200

    small change

--- WebApi/UploadFolder.cs
+++ WebApi/UploadFolder.cs
@@ -3,13 +3,8 @@
 public static class UploadFolder
 {
     //Hello there
-<<<<<<< HEAD
     public const string IMAGES = "\\\\192.168.0.200\\felix-share\\uploads\\images";
     public const string VIDEOS = @"C:\Users\felix\files\videos";
-=======
-    public const string IMAGES = @"\\192.168.0.200\felix-share\uploads\images\";
-    public const string VIDEOS = @"\\192.168.0.200\felix-share\uploads\videos\";
-    public const string THUMBNAILS = @"\\192.168.0.200\felix-share\uploads\thumbnails\";
->>>>>>> master
-    public const int MAX_FILE_SIZE = 1073741824;
+    public const string THUMBNAILS = @"C:\Users\felix\files\thumbnails";
+    public const int MAX_FILE_SIZE = 1073741824; //1GB
 }
