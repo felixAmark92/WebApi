7ea4405fd34971432773fcd330a5ecf45eeb995b
Felix Åmark <felix.amark@gmail.com>
2023-07-08 12:11:32 +0200

    sync

--- WebApi/UploadFolder.cs
+++ WebApi/UploadFolder.cs
@@ -4,7 +4,7 @@ public static class UploadFolder
 {
     //Hello there
     public const string IMAGES = "\\\\192.168.0.200\\felix-share\\uploads\\images";
-    public const string VIDEOS = @"C:\Users\felix\files\videos";
-    public const string THUMBNAILS = @"C:\Users\felix\files\thumbnails";
+    public const string VIDEOS = @"C:\Users\felix\files\videos\";
+    public const string THUMBNAILS = @"C:\Users\felix\files\thumbnails\";
     public const int MAX_FILE_SIZE = 1073741824; //1GB
 }
